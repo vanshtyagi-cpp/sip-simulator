b310c87d414c131ebbd394d51f70ea13d4c9d72f
vanshtyagi-cpp <vanshtyagii003@gmail.com>
2026-03-09 03:43:11 +0530

    Add files via upload

---
+++ sip_message.h
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class sipMessage
+{
+    private:
+    string from_header;
+    string  to_header;
+    int call_id;
+    int max;
+    string via;
+public:
+    void class_name() { cout <<"sipMessage"<< endl;} 
+};
